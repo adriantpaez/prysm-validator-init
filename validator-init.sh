db6310ca1c8bb8e63f16ded311209bb9f8b2e5c2
adriantpaez <adriantpaez@gmail.com>
2022-12-07 10:45:25 -0500

    feat: add import validator keys

--- validator-init.sh
+++ validator-init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Imprt validator keys
+./prysm.sh accounts import \
+    --accept-terms-of-use \
+    --keys-dir=/keystore/validator_keys \
+    --wallet-dir=/data/wallet \
+    --wallet-password-file=/keystore/keystore_password.txt \
+    --account-password-file=/keystore/keystore_password.txt
+
+# Import slashing protection data
 SLASHING_DATA_FILE=$1
 
 if [ -f "$SLASHING_DATA_FILE" ]; then
