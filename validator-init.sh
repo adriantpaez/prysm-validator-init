33bfa24c5d895964a3144a8a7c6ae0acfa45bf63
adriantpaez <adriantpaez@gmail.com>
2022-12-08 09:45:34 -0500

    feat: update validator-init

--- validator-init.sh
+++ validator-init.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-# Imprt validator keys
-./prysm.sh accounts import \
-    --accept-terms-of-use \
-    --keys-dir=/keystore/validator_keys \
-    --wallet-dir=/data/wallet \
-    --wallet-password-file=/keystore/keystore_password.txt \
-    --account-password-file=/keystore/keystore_password.txt
+# Parse arguments
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+# Import validator keys
+CMD="./prism.sh"
+
+if ! [ -z "$CHAIN_CONFIG_FILE" ]; then
+    CMD="$CMD --chain-config-file=$CHAIN_CONFIG_FILE"
+fi
+
+if ! [ -z "$NETWORK" ]; then
+    CMD="$CMD --$NETWORK"
+fi
+
+CMD="$CMD accounts import --accept-terms-of-use --keys-dir=/keystore/validator_keys --wallet-dir=/data/wallet --wallet-password-file=/keystore/keystore_password.txt --account-password-file=/keystore/keystore_password.txt"
+
+eval $CMD
 
 # Import slashing protection data
-SLASHING_DATA_FILE=$1
-
-if [ -f "$SLASHING_DATA_FILE" ]; then
+if ! [ -z "$SLASHING_DATA_FILE" ]; then
     echo "Found slashing interchange data in $SLASHING_DATA_FILE"
     echo "Sporting slashing data.."
     ./prysm.sh validator slashing-protection-history import --datadir=/data --slashing-protection-json-file=$SLASHING_DATA_FILE
